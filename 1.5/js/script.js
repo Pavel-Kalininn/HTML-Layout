0aba7f8fee4bed83e8f3116dc6e50513a095ab7c
Pavel-Kalininn <paul.kalininn@gmail.com>
2022-10-20 19:54:08 +0500

    1.5

--- 1.5/js/script.js
+++ 1.5/js/script.js
@@ -3,7 +3,7 @@ var expandBtn = document.querySelector('.expand-btn');
 
 expandBtn.addEventListener('click', function (evt) {
 	evt.preventDefault;
-	hidden.classList.toggle('hidden');
+	hidden.classList.toggle('swiper-wrapper--opened');
 	expandBtn.classList.toggle('shrink-btn');
 	if (expandBtn.innerText === 'Показать все') {
 		expandBtn.innerText = 'Скрыть'
--- 1.5/js/swiper.js
+++ 1.5/js/swiper.js
@@ -4,16 +4,14 @@ var swiper;
 function breakpointCheck() {
 	if (document.documentElement.clientWidth < 768) {
 		enableSwiper();
-		console.log('Включён');
 	}
 	if (document.documentElement.clientWidth >= 768 && swiper) {
 		swiper.destroy(true, true);
-		console.log('Выключен');
 	}
 }
 
 var enableSwiper = function () {
-	swiper = new Swiper(".mySwiper", {
+	swiper = new Swiper(".swiper", {
 		slidesPerView: "auto",
 		spaceBetween: 16,
 		pagination: {
